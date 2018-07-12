c05bfe3a3757fc3c24b0f8cc70cde3db3f879845
Andre Paris <andreparis@gmail.com>
2018-07-12 19:38:23 -0300

    register type

--- asm.go
+++ asm.go
@@ -1,5 +1,8 @@
 package asm
 
+//Register is a memory address of the NES hardware to read and/or write 
+type Register chan byte
+
 //Printf format a code with variable references
 func Printf(format string, v ...interface{}) {
 
