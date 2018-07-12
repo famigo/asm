5448e116126684b6ae1b0a405400fc92af6c3754
Andre Paris <andreparis@gmail.com>
2018-07-12 11:40:52 -0300

    first commit

---
+++ asm.go
@@ -0,0 +1,11 @@
+package asm
+
+//Printf format a code with variable references
+func Printf(format string, v ...interface{}) {
+
+}
+
+//Printfln do the same of Printf and appends a newline
+func Printfln(format string, v ...interface{}) {
+
+}
