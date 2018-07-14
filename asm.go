432cdcc0beea3672b72166822a92afeb2c6de6ca
Andre Paris <andreparis@gmail.com>
2018-07-14 10:50:01 -0300

    moved register to io

--- asm.go
+++ asm.go
@@ -1,8 +1,5 @@
 package asm
 
-//Register is a memory address of the NES hardware to read and/or write 
-type Register chan byte
-
 //Printf format a code with variable references
 func Printf(format string, v ...interface{}) {
 
